244fbbd528b31100b9cf2c83ab64fa396193979c
AhmedSamirSaadAli <94782348+AhmedSamirSaadAli@users.noreply.github.com>
2022-01-03 12:17:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "Samiiiiiiiiiiiiiiiiiiiiir")
+    name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
 if __name__ == "__main__":
